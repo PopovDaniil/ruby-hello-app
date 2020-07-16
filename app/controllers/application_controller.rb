8b74345f31e2a892f96151c11322d1d3e254a798
Daniil <pde-rnd@yandex.ru>
2020-07-16 22:22:55 +0300

    Упражнение 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
     def hello
         render plain: '¡Hola, mundo!'
     end
+    def goodbye
+        render plain: "<goodbye, world!"
+    end
 end
